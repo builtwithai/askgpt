1e461d1165a6f11e56fb5131cc0c054e1f3e5fa1
SKi <megamanics@users.noreply.github.com>
2023-04-05 14:52:04 -0700

    add test case

--- index.test.js
+++ index.test.js
@@ -6,15 +6,16 @@ dotnev.config();
 describe('getModelResponse', () => {
   test('returns a response from the model API', async () => {
     const url = process.env.ENDPOINT;
-    const apikey = process.env.KEY;
+    const apiKey = process.env.KEY;
     const prompt = 'Stock market analyst\'s analysis of ticker: MSFT Analysis:';
     const model = null;
     const temperature = 0.9;
+    const maxTokens = 500;
     const stop = '\n';
     const response = await getModelResponse(url, apiKey, prompt, model, maxTokens, temperature);
-    console.log(response);
+    console.debug(response);
     expect(response).toBeDefined();
-  });
+  }, 20000);
 });
 
 
