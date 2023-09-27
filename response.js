f4c0dd96ed590f5b1f837466b3de3b3e4803a6c2
SKi <megamanics@users.noreply.github.com>
2023-09-26 19:12:52 -0700

    type: perf: Improve response handling in `getModelResponse` function
    This commit enhances the `getModelResponse` function in `response.js`. The function now captures and handles potential errors during the completion generation process with a try-catch block, hence improving reliability. The message structure has been optimized to separate the prompts and responses into different messages, thereby improving organization and readability of the code. 🚀👌

--- response.js
+++ response.js
@@ -4,13 +4,20 @@ const AskLLM = require('AskLLM');
 async function getModelResponse(url, apiKey, model, prompts, options) {
   const llm = new AskLLM(url, apiKey, options);
   let response = "";
+  const messages = [];
 
   for (const prompt of prompts) {
-    const messages = [{ role: "user", content: `${response}${prompt}` }];
-    const completion = await llm.getCompletion(messages, options);
-    response += prompt + completion;
+    messages.push({ role: "user", content: `${prompt}` });
+    if (response !== "") {
+      messages.push({ role: "system", content: response });
+    }
+    try {
+      const completion = await llm.getCompletion(messages, options);
+      response = completion;
+    } catch (error) {
+      console.error(error);
+    }
   }
-
   return response;
 }
 
